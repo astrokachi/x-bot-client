3f7e7e53a34a2c281051ae81e57db225d02a6953
kachi <astrokachi@gmail.com>
2026-01-01 02:37:30 +0100

    add button component styles

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import "../styles/components/button.scss";
+import xIcon from "../assets/x.svg";
+import Spinner from "./spinner";
 
-const Button = ({children, onClick, disabled}: {children: React.ReactNode, onClick: () => void, disabled?: boolean}) => {
-  return (
-    <button className='login-btn' onClick={onClick} disabled={disabled}>{children}</button>
-  )
+interface ButtonProps {   
+  children: React.ReactNode;
+  onClick: () => void;
+  disabled?: boolean;
+  withIcon?: boolean;
+  isLoading?: boolean;
 }
+
+const Button = ({
+  children,
+  onClick,
+  disabled,
+  withIcon,
+  isLoading,
+}: ButtonProps) => {
+  return (
+    <button className="btn-con" onClick={onClick} disabled={disabled}>
+      {withIcon && <img src={xIcon} width={12} />}
+      <span className={`login-btn`}>{isLoading ? <Spinner /> : children}</span>
+    </button>
+  );
+};
 
 export default Button;
